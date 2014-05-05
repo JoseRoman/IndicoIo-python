c8ffe3629f85dce09e759ea0dd0be61266499b21
Charlotte-Sommerville <charlotteisthekey@gmail.com>
2014-05-05 11:08:14 -0400

    added packages

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ except ImportError:
 
 setup(
 	name = "IndicoIo",
-	version = '0.2.3',
+	version = '0.2.4',
 	packages = ["IndicoIo",],
 	license = "MIT License (See LICENSE)",
 	long_description = open("README").read(),
 	url = "https://github.com/IndicoDataSolutions/IndicoIo-python",
 	author = "Alec Radford, Slater Victoroff",
 	author_email = "Alec Radford <alec@indicodatasolutions.com>, Slater Victoroff <slater@indicodatasolutions.com>",
+        py_modules = ['IndicoIo', 'IndicoIo.text', 'IndicoIo.images']
 )
