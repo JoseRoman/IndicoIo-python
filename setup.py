62a2392fc930c7fe60a7f69884423ed659e5ff90
Slater-Victoroff <slater.r.victoroff@gmail.com>
2014-05-05 00:59:26 -0400

    Hopefully fixed markdown

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	version = '0.2.0',
 	packages = ["IndicoIo",],
 	license = "MIT License (See LICENSE)",
-	long_description = open("README").read(),
+	long_description = open("README.md").read(),
 	url = "http://www.indico.io",
 	author = "Alec Radford, Slater Victoroff",
 	author_email = "Alec Radford <alec@indicodatasolutions.com>, Slater Victoroff <slater@indicodatasolutions.com>",
