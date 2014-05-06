9494e4cb34f989b44e34150219e9b028e8b1a55e
Charlotte-Sommerville <charlotteisthekey@gmail.com>
2014-05-05 17:13:41 -0700

    modified setup.py to include proper packages

--- setup.py
+++ setup.py
@@ -6,11 +6,17 @@ except ImportError:
 setup(
 	name = "IndicoIo",
 	version = '0.2.5',
-	packages = ["IndicoIo",],
+	packages = [
+            "IndicoIo",
+            "IndicoIo.text.sentiment",
+            "IndicoIo.images.fer",
+            "IndicoIo.images.facial_features",
+            "IndicoIo.text",
+            "IndicoIo.images"
+        ],
 	license = "MIT License (See LICENSE)",
 	long_description = open("README").read(),
 	url = "https://github.com/IndicoDataSolutions/IndicoIo-python",
 	author = "Alec Radford, Slater Victoroff",
 	author_email = "Alec Radford <alec@indicodatasolutions.com>, Slater Victoroff <slater@indicodatasolutions.com>",
-        py_modules = ['IndicoIo.text.sentiment', 'IndicoIo.images.fer', 'IndicoIo.images.facial_features']
 )
